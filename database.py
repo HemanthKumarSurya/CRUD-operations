0ee147df147df55dd0780bb7637d4ee53f94b77b
Hemanth Kumar KS <suryahemanth909@gmail.com>
2025-11-18 12:51:42 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+db_url="postgresql://postgres:9003@localhost:5432/affine"
+engine = create_engine(db_url)
+session = sessionmaker(autocommit=False, autoflush=False ,bind=engine)
